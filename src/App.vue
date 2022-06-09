3bc5c5a21b7d6d6c6b8fffa8a5ba02a75e285860
minatomisako24 <minatomisako24@icloud.com>
2022-06-09 11:07:02 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+      <router-view v-slot="{ Component, route }">
+          <transition name="fade" mode="out-in">
+              <component :is="Component" :key="route.path"></component>
+          </transition>
+      </router-view>
+      <div class="live2d-menu">
+      </div>
+  </div>
 </template>
 
+<style scoped lang="scss">
+    .fade-enter-form, .fade-leave-to {
+        // transform: translateX(20px);
+        opacity: 0;
+    }
+    .fade-enter-to, .fade-leave-from {
+        opacity: 1;
+    }
+    .fade-enter-active {
+        transition: all 0.7s ease;
+    }
+    .fade-leave-active {
+        transition: all 0.3s cubic-bezier(1, 0.6, 0.6, 1);
+    }
+</style>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        padding: 0;
+        margin: 0;
+      }
+    html {
+        min-width: 1227px;
+    }
 </style>
---
+++ src/components/Concat.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <el-container>
+            <el-header class="header">
+                <el-affix>
+                    <BaseHeader></BaseHeader>
+                </el-affix>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                </el-aside>
+                <el-main></el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .header, .footer {
+        padding: 0;
+    }
+    ::v-deep(.header) {
+        .el-affix--fixed {
+            background-color: #ffffff;
+        }
+    }
+    .main {
+        padding: 0;
+    }
+</style>
